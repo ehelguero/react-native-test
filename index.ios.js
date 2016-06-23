e171fc1914fe137377475eaf2d4e931d0c006cb7
loupgarou81 <leloupgarou81@gmail.com>
2016-06-23 12:39:32 +0100

    Added data to ListView

--- index.ios.js
+++ index.ios.js
@@ -24,7 +24,10 @@ class AwesomeProject extends Component {
   constructor(props){
     super(props);
     this.state = {
-      movies: null,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
     }
   }
   
@@ -37,19 +40,25 @@ class AwesomeProject extends Component {
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
-          movies: responseData.movies,
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
         });
     })
     .done();
   }
   
   render() {
-    if(!this.state.movies){
+    if(!this.state.dataSource){
       return this.renderLoadingView();
     }
     
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
+   return (
+     <ListView
+       dataSource={this.state.dataSource}
+       renderRow={this.renderMovie}
+       style={styles.listView}
+      />
+   )
   }
   
   renderLoadingView(){
@@ -104,6 +113,10 @@ var styles = StyleSheet.create({
   year: {
     textAlign: 'center',
   },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
