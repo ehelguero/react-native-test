a87b9098ca765d04a457b0f4cdd9d38581e87cf3
loupgarou81 <leloupgarou81@gmail.com>
2016-06-23 12:24:10 +0100

    Fetching data

--- index.ios.js
+++ index.ios.js
@@ -17,25 +17,65 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 class AwesomeProject extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: null,
+    }
+  }
+  
+  componentDidMount(){
+    this.fetchData();
+  }
+  
+  fetchData(){
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+    })
+    .done();
+  }
+  
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if(!this.state.movies){
+      return this.renderLoadingView();
+    }
+    
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+  
+  renderLoadingView(){
     return (
-      <View style={styles.container}>
-          <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rigthContainer}>
-          <Text style={styles.title}>
-            {movie.title}
-          </Text>
-          <Text style={styles.year}>
-            {movie.year}
-          </Text>
+        <View style={styles.container}>
+           <Text>Loading movies...</Text>
         </View>
-      </View>
-    );
+      );
+  }
+
+  renderMovie(movie){
+    return (
+        <View style={styles.container}>
+            <Image
+            source={{uri: movie.posters.thumbnail}}
+            style={styles.thumbnail}
+          />
+          <View style={styles.rigthContainer}>
+            <Text style={styles.title}>
+              {movie.title}
+            </Text>
+            <Text style={styles.year}>
+              {movie.year}
+            </Text>
+          </View>
+        </View>
+      );
   }
 }
 
